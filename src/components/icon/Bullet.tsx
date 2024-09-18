8e4af4c0cf2663e1b396af26b65b4450347700af
anmst21 <anmstudios21c@gmail.com>
2024-09-18 10:05:26 -0400

    finished styling

---
+++ src/components/icon/Bullet.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      fill="none"
+      viewBox="0 0 14 14"
+    >
+      <path
+        fill="currentColor"
+        d="M0 2.914v8.172c0 .89 1.077 1.337 1.707.707L6.5 7 1.707 2.207C1.077 1.577 0 2.023 0 2.914z"
+      ></path>
+      <path
+        stroke="currentColor"
+        strokeWidth="2"
+        d="M13 13.094V2.914c0-.89-1.077-1.337-1.707-.707L6.5 7m0 0l-4.793 4.793c-.63.63-1.707.184-1.707-.707V2.914c0-.89 1.077-1.337 1.707-.707L6.5 7z"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,4 +1,5 @@
 import Globe from "./Globe";
 import ChevDown from "./ChevDown";
+import Bullet from "./Bullet";
 
-export { Globe, ChevDown };
+export { Globe, ChevDown, Bullet };
