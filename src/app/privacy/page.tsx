be2ab3fd886e31c61e36ed23cdf4bd4f438ab651
anmst21 <anmstudios21c@gmail.com>
2024-11-02 14:23:09 -0400

    cache clear

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -7,7 +7,7 @@ export default function Privacy() {
         <div className="privacy__navigation">
           <Nexus />
           <span className="privacy__navigation__team">
-            KEK
+            H3LLCAT
             <span className="privacy__navigation__responsive">Team</span>
           </span>
           <div className="privacy__navigation__language">
