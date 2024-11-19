3a37c9e650ea19128481ca0e87ca2c3b9a1688b9
anmst21 <anmstudios21c@gmail.com>
2024-11-19 13:33:02 -0500

    1

--- src/components/collection/bg-images.tsx
+++ src/components/collection/bg-images.tsx
@@ -5,30 +5,31 @@ import crystallImg from "@/app/collection-img-crystall.png";
 
 const BgImages = () => {
   return (
-      <>
-          <Image
-              className="pyramid-img"
-              src={pyramidImg}
-              width={192.12}
-              height={192.12}
-              alt="pyramid"
-          />
-          <Image
-              className="leaf-img"
-              src={leafImg}
-              width={191.66}
-              height={191.66}
-              alt="leaf"
-          />
-          <Image
-              className="crystall-img"
-              src={crystallImg}
-              width={262.62}
-              height={262.62}
-              alt="crystall"
-          />
-      </>
+    <>
+      <Image
+        className="pyramid-img"
+        src={pyramidImg}
+        width={192.12}
+        height={192.12}
+        alt="pyramid"
+      />
+      <Image
+        className="leaf-img"
+        src={leafImg}
+        width={191.66}
+        height={191.66}
+        alt="leaf"
+      />
+      <Image
+        className="crystall-img"
+        src={crystallImg}
+        width={262.62}
+        height={262.62}
+        alt="crystall"
+      />
+    </>
   );
 };
 
 export default BgImages;
+//
