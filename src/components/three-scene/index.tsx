e6f4da16bef2df37ad13e264dd9e9c70edbb6198
Alexander Emelyanov <35880220+Homosanians@users.noreply.github.com>
2024-11-19 21:52:12 +0300

    new fbxes & remove complex shader

--- src/components/three-scene/index.tsx
+++ src/components/three-scene/index.tsx
@@ -27,102 +27,29 @@ function AnimatedFBX({
       const mesh = child as THREE.Mesh;
       mesh.material = new THREE.ShaderMaterial({
         vertexShader: `
-          uniform float iTime;
-          varying vec3 vPosition;
-          varying vec3 vNormal;
-          varying vec2 vUv;
-      
-          float noise(vec3 p) {
-            return sin(p.x * 1.0 + p.y * 3.0 + p.z * 1.0 + iTime) * 0.5 + 0.5;
-          }
-      
+          varying vec2 vUv; 
           void main() {
-            vUv = uv;
-            vec3 distortedPosition = position + normal * noise(position + sin(iTime)) * 0.5;
-            vPosition = distortedPosition;
-            vNormal = normalize(normalMatrix * normal);
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(distortedPosition, 1.0);
+            vUv = uv; 
+            vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+            gl_Position = projectionMatrix * modelViewPosition; 
           }
         `,
         fragmentShader: `
           uniform vec2 iResolution;
           uniform float iTime;
-
-          varying vec3 vNormal;
-          varying vec3 vPosition;
           varying vec2 vUv;
-
-          float randomizedTime(float baseTime, vec2 uv) {
-              float randomOffset = fract(sin(dot(uv * 123.45, vec2(78.233, 45.543))) * 43758.5453123);
-              return baseTime + randomOffset * 2.0;
-          }
-
-          float opSmoothUnion(float d1, float d2, float k) {
-              float h = clamp(0.5 + 0.5 * (d2 - d1) / k, 0.0, 1.0);
-              return mix(d2, d1, h) - k * h * (1.0 - h);
-          }
-
-          float sdSphere(vec3 p, float s) {
-              return length(p) - s;
-          }
-
-          float map(vec3 p) {
-              float d = 2.0;
-              for (int i = 0; i < 16; i++) {
-                  float fi = float(i);
-                  float rTime = randomizedTime(iTime, vUv);
-                  float time = rTime * (fract(fi * 412.531 + 0.513) - 0.5) * 2.0;
-                  d = opSmoothUnion(
-                      sdSphere(p + sin(time + fi * vec3(52.5126, 64.62744, 632.25)) * vec3(2.0, 2.0, 0.8), mix(0.5, 1.0, fract(fi * 412.531 + 0.5124))),
-                      d,
-                      0.4
-                  );
-              }
-              return d;
-          }
-
-          vec3 calcNormal(in vec3 p) {
-              const float h = 1e-5;
-              const vec2 k = vec2(1, -1);
-              return normalize(
-                  k.xyy * map(p + k.xyy * h) +
-                  k.yyx * map(p + k.yyx * h) +
-                  k.yxy * map(p + k.yxy * h) +
-                  k.xxx * map(p + k.xxx * h)
-              );
-          }
 
           void main() { 
               vec2 uv = vUv;
 
-              vec3 rayOri = vec3((uv - 0.5) * vec2(iResolution.x / iResolution.y, 1.0) * 6.0, 3.0);
-              vec3 rayDir = vec3(0.0, 0.0, -1.0);
-
-              float depth = 0.0;
-              vec3 p;
-
-              for (int i = 0; i < 64; i++) {
-                  p = rayOri + rayDir * depth;
-                  float dist = map(p);
-                  depth += dist;
-                  if (dist < 1e-6) {
-                      break;
-                  }
-              }
-
-              depth = min(6.0, depth);
-              vec3 n = calcNormal(p);
-              float b = max(0.0, dot(n, vec3(0.577)));
-              vec3 col = (0.5 + 0.5 * cos((b + iTime * 3.0) + uv.xyx * 700.0 + vec3(0, 2, 4))) * (0.85 + b * 0.35);
-              // orig
-              // vec3 col = (0.5 + 0.5 * cos((b + iTime * 3.0) + uv.xyx * 2.0 + vec3(0, 2, 4))) * (0.85 + b * 0.35);
-              col *= exp(-depth * 0.15);
-
-              gl_FragColor = vec4(col, 1.0 - (depth - 0.5) / 2.0);
+              vec3 col = 0.5 + 0.5 * cos(iTime + uv.xyx * vec3(0.5, 0.8, 1.2));
+              gl_FragColor = vec4(col, 1.0);
           }
         `,
         uniforms: {
-          iResolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
+          iResolution: {
+            value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+          },
           iTime: { value: 0.0 },
         },
       });
@@ -179,9 +106,9 @@ function Scene() {
         <pointLight position={[-3, 0, -5]} intensity={1}/>
 
         {/* FBX Objects */}
-        <AnimatedFBX path="/assets/three/Sphere.fbx" position={[-10, 18, 0]} scaleFactor={0.013}/>
-        <AnimatedFBX path="/assets/three/Sphere.fbx" position={[-13, 1, 0]} scaleFactor={0.017}/>
-        <AnimatedFBX path="/assets/three/Sphere.fbx" position={[7, 11, 0]} scaleFactor={0.017}/>
+        <AnimatedFBX path="/assets/three/1.fbx" position={[-10, 18, 0]} scaleFactor={0.017}/>
+        <AnimatedFBX path="/assets/three/2.fbx" position={[-11, 1, 0]} scaleFactor={0.017}/>
+        <AnimatedFBX path="/assets/three/3.fbx" position={[11, 11, 0]} scaleFactor={0.017}/>
         {/* Grid Helper */}
         {/*<gridHelper args={[30, 30]} />*/}
         {/* Camera LookAt Logic */}
