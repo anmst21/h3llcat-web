46216562d9ea7f472c45a6a37a6b754554545e05
anmst21 <anmstudios21c@gmail.com>
2025-02-04 22:31:54 -0500

    reverted capcha badge

--- src/helpers/apiUri.ts
+++ src/helpers/apiUri.ts
@@ -2,3 +2,6 @@ export const apiUriSubmitMint =
   "https://api-airdrop.h3llcat.app/api/v1/privyforms/submit/mint";
 
 export const apiUri = "https://api-airdrop.h3llcat.app/api/v1/privyforms/mints";
+
+export const apiUriSubmitBeta =
+  "https://api-airdrop.h3llcat.app/api/v1/privyforms/submit/email";
