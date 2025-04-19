cb9c05e0df28f0cab6947479856a6b2cf1cf2bee
anmst21 <anmstudios21c@gmail.com>
2025-04-18 21:17:58 -0400

    finished redirects => test

--- src/app/(website)/collection/layout.tsx
+++ src/app/(website)/collection/layout.tsx
@@ -1,22 +1,24 @@
 import PreviewBackground from "@/components/app-redirect/preview-background";
+import posts from "../../../../assets-redirect.json";
+import { Collection } from "@/types/CollectionCarousel";
 
 export default async function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const res = await fetch("http://localhost:3000/assets-redirect.json");
-  // ↳ replace localhost:3000 with your VERCEL_URL or process.env if needed
-  if (!res.ok) {
-    throw new Error("Could not fetch redirects JSON");
-  }
+  // const res = await fetch("/assets-redirect.json");
+  // // ↳ replace localhost:3000 with your VERCEL_URL or process.env if needed
+  // if (!res.ok) {
+  //   throw new Error("Could not fetch redirects JSON");
+  // }
 
-  const redirects = await res.json();
+  // const redirects = await res.json();
   return (
     <div className="nft">
       {children}
 
-      <PreviewBackground redirects={redirects} />
+      <PreviewBackground redirects={posts as Collection[]} />
     </div>
   );
 }
